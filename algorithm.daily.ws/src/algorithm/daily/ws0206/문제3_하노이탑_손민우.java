67b2e4fdf65e6288504a0bb31b6fb7a656eabeba
yoyoyo09094 <yoyoyo09094@gmail.com>
2023-02-07 09:13:48 +0900

    20230207

--- algorithm.daily.ws/src/algorithm/daily/ws0206/문제3_하노이탑_손민우.java
+++ algorithm.daily.ws/src/algorithm/daily/ws0206/문제3_하노이탑_손민우.java
@@ -1,9 +1,29 @@
 package algorithm.daily.ws0206;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class 문제3_하노이탑_손민우 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		char startPoint = 'A';
+		char otherPoint = 'B';
+		char endPoint = 'C';
+		solution(n,startPoint, endPoint, otherPoint);
+	}
+	
+	private static void solution(int n, char startPoint, char endPoint, char otherPoint) {
+		if(n==1) {
+			System.out.println(n+"이" +startPoint +"에서 "+ endPoint+"로 이동하였다.");
+			return;
+		}
+		else {
+			solution(n-1, startPoint, otherPoint, endPoint);
+			System.out.println(n+"이" +startPoint +"에서 "+ endPoint+"로 이동하였다.");
+			solution(n-1, otherPoint, endPoint, startPoint);
+		}
 	}
 
 }
