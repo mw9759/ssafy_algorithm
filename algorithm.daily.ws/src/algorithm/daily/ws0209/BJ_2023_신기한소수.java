bd63b7de2f07112990f81c2ff34f07cfc43c3399
Minwoo Son <yoyoyo09094@gmail.com>
2023-02-10 01:59:58 +0900

    20230210

--- algorithm.daily.ws/src/algorithm/daily/ws0209/BJ_2023_신기한소수.java
+++ algorithm.daily.ws/src/algorithm/daily/ws0209/BJ_2023_신기한소수.java
@@ -3,22 +3,39 @@ package algorithm.daily.ws0209;
 import java.util.Scanner;
 
 public class BJ_2023_신기한소수 {
-
-	static int n;
 	
+	static int n;
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		int startN = (int) Math.pow(10, n-1);
-		solution(startN);
+		
+		int primeNum[] = {2, 3, 5, 7};
+		
+		for(int i : primeNum) {
+			solution(i, 1);
+		}
+		System.out.println(sb);
 	}
 	
-	private static void solution(int startN) {
-		for(int i = startN+1; i<Math.pow(10, n); i+=2) {
-			int x = (int)Math.sqrt(i);
-			System.out.println(x);
+	private static void solution(int primeNum, int cnt) {
+		if(cnt == n) {
+			sb.append(primeNum).append("\n");
+			return;
+		}
+		
+		for(int i = 1; i<10; i+=2) {
+			if(isPrime(primeNum*10+i)) solution(primeNum*10+i, cnt+1);
 		}
 	}
+
+	private static boolean isPrime(int num) {
+		for(int i = 2; i<=Math.sqrt(num); i++) {
+			if(num%i == 0) return false;
+		}
+		return true;
+	}
 	
-	
+		
 }
+
