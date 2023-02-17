30f875914deb308a929a7a68241c50b0ed4c7896
null <null>
2023-02-17 17:45:51 +0900

    20230217

--- algorithm.daily.ws/src/algorithm/daily/ws0215/BJ_16935_배열돌리기3_손민우.java
+++ algorithm.daily.ws/src/algorithm/daily/ws0215/BJ_16935_배열돌리기3_손민우.java
@@ -24,13 +24,32 @@ public class BJ_16935_배열돌리기3_손민우 {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		System.out.println(Arrays.deepToString(arr));
-		//p1();
-		//p2();
-		p3();
-		System.out.println(Arrays.deepToString(arr));
-		p4();
-		System.out.println(Arrays.deepToString(arr));
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<c; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			switch (a) {
+			case 1: p1();
+					break;
+			case 2: p2();
+				break;
+			case 3: p3();
+				break;
+			case 4: p4();
+				break;
+			case 5: p5();
+				break;
+			case 6: p6();
+				break;
+			}
+		}
+		// 출력
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<m; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 	
 	private static void p1() {
@@ -66,15 +85,46 @@ public class BJ_16935_배열돌리기3_손민우 {
 		m = ttmp;
 	}
 	private static void p4() {
-		int arr2[][] = new int[m][n];
-		for(int j=m-1;j>=0;j--) {
-			for(int i=0;i<n;i++)
-				arr2[m-1-j][i] = arr[i][j];
+		for(int i = 0; i<3; i++) {
+			p3();
 		}
+	}
+	private static void p5() {
+		int arr2[][] = new int[n][m];
+		//좌하-> 좌상
+		for(int i = 0; i<n/2; i++) {
+			for(int j = 0; j<m/2;j++) {
+				arr2[i][j] = arr[i+n/2][j];
+			}
+		}
+		
+		//좌상-> 우상
+		for(int i = 0; i<n/2; i++) {
+			for(int j = m/2; j<m;j++) {
+				arr2[i][j] = arr[i][j-m/2];
+			}
+		}
+		
+		//우상-> 우하
+		for(int i = n/2; i<n; i++) {
+			for(int j = m/2; j<m;j++) {
+				arr2[i][j] = arr[i-n/2][j];
+			}
+		}
+		
+		//우하-> 좌하
+		for(int i = n/2; i<n; i++) {
+			for(int j = 0; j<m/2;j++) {
+				arr2[i][j] = arr[i][j+m/2];
+			}
+		}
+		
 		arr=arr2;
-		int tmp = n;
-		n = m;
-		n = tmp;
 	}
 	
+	private static void p6() {
+		for(int i = 0; i<3; i++) {
+			p5();
+		}
+	}
 }
